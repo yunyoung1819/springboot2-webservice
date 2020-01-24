d1c09f2a1ea8f6b03263e5defc9bc8dbc0fd6149
yunyoung1819 <yunyoung1819@hanmail.net>
2020-01-24 21:58:14 +0900

    게시판 만들기 crud 기능 구현

---
+++ src/main/java/com/young/book/springboot/web/PostsApiController.java
@@ -0,0 +1,22 @@
+package com.young.book.springboot.web;
+
+import com.young.book.springboot.service.posts.PostsService;
+import com.young.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PutMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
+        return postsService.save(requestDto);
+
+    }
+}
