574cee37c272087bad410c3abfb0ae6ac0d3e969
yunyoung1819 <yunyoung1819@hanmail.net>
2020-01-31 01:08:37 +0900

    게시판 만들기 crud 기능 구현

--- src/main/java/com/young/book/springboot/web/PostsApiController.java
+++ src/main/java/com/young/book/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.young.book.springboot.web;
 
-import com.young.book.springboot.service.posts.PostsService;
+import com.young.book.springboot.service.PostsService;
+import com.young.book.springboot.web.dto.PostsResponseDto;
 import com.young.book.springboot.web.dto.PostsSaveRequestDto;
+import com.young.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,10 +13,21 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    // 등록 기능
     @PutMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
-
         return postsService.save(requestDto);
+    }
 
+    // 수정 기능
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    // 조회 기능
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
