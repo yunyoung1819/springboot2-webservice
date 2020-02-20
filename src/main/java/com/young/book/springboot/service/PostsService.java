13732952b910d0347c6f013cb611591453370c3a
yunyoung1819 <yunyoung1819@hanmail.net>
2020-02-21 00:58:02 +0900

    게시판 만들기 crud 기능 구현

--- src/main/java/com/young/book/springboot/service/PostsService.java
+++ src/main/java/com/young/book/springboot/service/PostsService.java
@@ -2,13 +2,16 @@ package com.young.book.springboot.service;
 
 import com.young.book.springboot.domain.posts.Posts;
 import com.young.book.springboot.domain.posts.PostsRepository;
+import com.young.book.springboot.web.dto.PostsListResponseDto;
 import com.young.book.springboot.web.dto.PostsResponseDto;
 import com.young.book.springboot.web.dto.PostsSaveRequestDto;
 import com.young.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +35,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
