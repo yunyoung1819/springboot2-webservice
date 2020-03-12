ef958e4e63621daaf9a2fe495c444a6a0a046970
yunyoung1819 <yunyoung1819@hanmail.net>
2020-03-13 08:01:53 +0900

    chapter5. 소셜 로그인 구현

---
+++ src/main/java/com/young/book/springboot/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,4 @@
+package com.young.book.springboot.config.auth;
+
+public class CustomOAuth2UserService {
+}
---
+++ src/main/java/com/young/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.young.book.springboot.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+
+}
