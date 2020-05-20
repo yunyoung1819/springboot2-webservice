8bfd7e7f4a026b43f97a4645411c16fcac884cd7
yunyoung1819 <yunyoung1819@hanmail.net>
2020-05-21 02:14:08 +0900

    chapter5. 소셜 로그인 구현

--- src/main/java/com/young/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/young/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -1,5 +1,7 @@
 package com.young.book.springboot.config.auth.dto;
 
+import com.young.book.springboot.domain.user.Role;
+import com.young.book.springboot.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -27,5 +29,24 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId, String userNameAttributeName,
                                      Map<String, Object> attributes) {
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
     }
 }
